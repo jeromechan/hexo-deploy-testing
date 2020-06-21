16484e6fa135249411af6c2879ad67dd027f77f8
jeromechan <jerome.chan369@gmail.com>
2020-06-21 15:39:05 +0800

    Auto reset master files using hexo-deploy.sh script.

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,7 @@ git push > /dev/null 2>&1
 
 # 3. 将public文件夹内容拷贝至/tmp/jeromechan.github.io，额外包含项目目录下的"CNAME"文件
 #mkdir $REPO_NAME > /dev/null 2>&1
+rm -rf /tmp/$REPO_NAME
 cp -r ./public/ /tmp/$REPO_NAME
 cp ./CNAME /tmp/$REPO_NAME/
 
@@ -27,6 +28,7 @@ git commit -am 'Auto reset master files using hexo-deploy.sh script.'
 
 # 5. 将/tmp/jeromechan.github.io文件内容全部拷贝至master分支下
 cp -r /tmp/$REPO_NAME ./
+rm -rf /tmp/$REPO_NAME
 
 # 6. 执行git add * & git commit -am 'Refresh and add master files using hexo-deploy.sh script.' & git push
 git add * > /dev/null 2>&1
--- hexo-deploy.sh
+++ hexo-deploy.sh
@@ -15,7 +15,13 @@ BRANCH_MASTER="master"
 # git commit -am 'Auto update file using hexo-deploy.sh script.'
 # git push
 
-# 3. 将public文件夹内容拷贝至/tmp/jeromechan.github.io，额外包含项目目录下的"CNAME"文件
-#mkdir $REPO_NAME > /dev/null 2>&1
-cp -r ./public/ /tmp/$REPO_NAME
-cp ./CNAME /tmp/$REPO_NAME/
+# # 3. 将public文件夹内容拷贝至/tmp/jeromechan.github.io，额外包含项目目录下的"CNAME"文件
+# #mkdir $REPO_NAME > /dev/null 2>&1
+# rm -rf /tmp/$REPO_NAME
+# cp -r ./public/ /tmp/$REPO_NAME
+# cp ./CNAME /tmp/$REPO_NAME/
+
+# 4. 切换至master分支下，删除所有非隐藏的项目文件，然后执行git commit -am 'Auto reset master files using hexo-deploy.sh script.'
+git checkout $BRANCH_MASTER
+rm -rf .
+git commit -am 'Auto reset master files using hexo-deploy.sh script.'
